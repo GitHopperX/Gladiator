873d9aab4f04055f565a5a6eef53d00949ce9364
Hasib R Rahman <hasibkyau.cse@gmail.com>
2021-05-28 03:09:36 +0600

    updated the car code

--- gladiator/gladiator.ino
+++ gladiator/gladiator.ino
@@ -1,46 +1,110 @@
 #include <AFMotor.h>
 #include <Servo.h>
 
-// DC motor on M2
-
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
-
-
-// DC hobby servo
 Servo servo1;
-// Stepper motor on M3+M4 48 steps per revolution
 
 void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Motor party!");
+  Serial.begin(9600);// set up Serial library at 9600 bps
 
   // turn on servo
   servo1.attach(9);
 
-  // turn on motor #2
-  motor1.setSpeed(200);
+  // turn on DC motor
+  motor1.setSpeed(55);
   motor1.run(RELEASE);
-  motor2.setSpeed(200);
+  motor2.setSpeed(55);
   motor2.run(RELEASE);
 
 }
 
 int i;
 char data = "";
-// Test the DC motor, stepper and servo ALL AT ONCE!
+int spd, key = 0;
+
 void loop() {
-  if (Serial.available() > 0){data = Serial.read();}
+  //Communication With Car
+  if (Serial.available() > 0) {
+    data = Serial.read();
+  }
   Serial.println(data);
 
-    if (data == 'L') {
-      motor1.run(RELEASE); //FORWARD
-      motor2.run(FORWARD); //FORWARD
-    }
-
-     if (data == 'R') {
-      motor2.run(RELEASE); //FORWARD
-      motor1.run(FORWARD); //FORWARD
-    }
-
+  //Car Speed control
+  if (data >= '0' && data <= '9') {
+    spd = data;
+    spd = data - 47;
+    spd = (spd * 20) + 55;
+    Serial.println(spd);
+    //delay(1000);
+    motor1.setSpeed(spd);
+    motor2.setSpeed(spd);
   }
+  
+  //Car Start
+  if (data == 'X'){
+    key = 1;
+  }
+  if (data == 'x'){
+    key = 0;
+  }
+
+  //Car Drive
+  if (key == 1) {
+    switch (data) {
+      case 'L': //LEFT
+        motor1.setSpeed(spd);
+        motor2.setSpeed(spd);
+        motor1.run(RELEASE);
+        motor2.run(FORWARD);
+        break;
+      case 'R': //RIGHT
+        motor1.setSpeed(spd);
+        motor2.setSpeed(spd);
+        motor2.run(RELEASE);
+        motor1.run(FORWARD);
+        break;
+      case 'B': //BACK
+        motor1.setSpeed(spd);
+        motor2.setSpeed(spd);
+        motor1.run(BACKWARD);
+        motor2.run(BACKWARD);
+        break;
+      case 'F': //FORWARD
+        motor1.setSpeed(spd);
+        motor2.setSpeed(spd);
+        motor1.run(FORWARD);
+        motor2.run(FORWARD);
+        break;
+      case 'I': //FORWARD RIGHT
+        motor1.setSpeed(spd);
+        motor2.setSpeed(spd - spd / 2);
+        motor1.run(FORWARD);
+        motor2.run(FORWARD);
+        break;
+      case 'J': //BACKWARD RIGHT
+        motor1.setSpeed(spd);
+        motor2.setSpeed(spd - spd / 2);
+        motor1.run(BACKWARD);
+        motor2.run(BACKWARD);
+        break;
+      case 'G': //FORWARD LEFT
+        motor1.setSpeed(spd - spd / 2);
+        motor2.setSpeed(spd);
+        motor1.run(FORWARD);
+        motor2.run(FORWARD);
+        break;
+      case 'H': //BACKWARD LEFT
+        motor1.setSpeed(spd - spd / 2);
+        motor2.setSpeed(spd);
+        motor1.run(BACKWARD);
+        motor2.run(BACKWARD);
+        break;
+      case 'S': //STOP
+        motor1.setSpeed(spd);
+        motor2.setSpeed(spd);
+        motor1.run(RELEASE);
+        motor2.run(RELEASE);
+    }
+  }
+}
